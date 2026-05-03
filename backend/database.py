c6ed20f782607b7f8ab83b0de4bcf62c56fed64e
learningsam20 <learningsam20@gmail.com>
2026-05-04 01:35:04 +0530

    updated validations

--- backend/database.py
+++ backend/database.py
@@ -1,3 +1,6 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from backend.core.config import settings
 
 # We expect SQLite path like sqlite:///./election.db
